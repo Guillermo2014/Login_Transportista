9fbf2d1aa28c33f46238279b5b0909ba5db47881
Guillermo <aries_20_15@hotmail.com>
2021-04-25 00:07:29 -0500

    login

---
+++ Logictrack_listado/Logictrack_listado/Controllers/DespachoController.cs
@@ -0,0 +1,35 @@
+﻿using Logictrack_listado.API;
+using Logictrack_listado.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Logictrack_listado.Controllers
+{
+    public class DespachoController : System.Web.Mvc.Controller
+    {
+        Backend _api = new Backend();
+        // GET: Despacho
+        public async Task<System.Web.Mvc.ActionResult> Index()
+        {
+            List<Despacho> despachos = new List<Despacho>();
+            HttpClient client = _api.Initial();
+            //HttpResponseMessage response = await client.GetAsync("despachosPorTransportista" + $"/{id}");
+            var uri = "despachosPorTransportista/" + HttpContext.Session["IdTransportista"].ToString();
+            HttpResponseMessage response = await client.GetAsync(uri);
+            //HttpResponseMessage response = await client.GetAsync("despachos");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                despachos = JsonConvert.DeserializeObject<List<Despacho>>(result);
+            }
+            return View(despachos);
+        }
+    }
+}
